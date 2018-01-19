6bc491cceadc247dcb55c5f1590feb20c16e2bb0
mofas223 <mofas223@gmail.com>
2018-01-18 21:32:33 -0500

    finish lab3

--- lab3.c
+++ lab3.c
@@ -16,6 +16,13 @@ void printArray(int arr[], int length)
   printf("]\n");
 }
 
+void swap(int *a, int *b)
+{
+  int tmp = *a;
+  *a = *b;
+  *b = tmp;
+}
+
 int randRange(int from, int to)
 {
   int r = rand();
@@ -37,8 +44,23 @@ int randRange(int from, int to)
 // }
 // swap(A[i + 1], A[r]) return i + 1 // return the new postion of the pivot element
 
-void randomizedPartition(int array[], int p, int r)
+int randomizedPartition(int array[], int p, int r)
 {
+  int k = randRange(p, r);
+  swap(&array[k], &array[r]);
+  int x = array[r];
+  int i = p - 1;
+  // printf("k=%d, x=%d, p=%d, i=%d\n", k, x, p, i);
+  for (int j = p; j < r; j++)
+  {
+    if (array[j] <= x)
+    {
+      i++;
+      swap(&array[i], &array[j]);
+    }
+  }
+  swap(&array[i + 1], &array[r]);
+  return i + 1;
 }
 
 // RANDOMIZED_QUICK_SORT(A, p, r) : if p < r : q = RANDOMIZED_PARTITION(A, p, r)
@@ -47,12 +69,38 @@ void randomizedPartition(int array[], int p, int r)
 
 void randomizedQucikSort(int array[], int p, int r)
 {
+  if (p < r)
+  {
+    int q = randomizedPartition(array, p, r);
+    // printf("p=%d, q=%d r=%d\n", p, q, r);
+    randomizedQucikSort(array, p, q - 1);
+    randomizedQucikSort(array, q + 1, r);
+  }
 }
 
 int main()
 {
   srand(time(NULL)); // should only be called once
 
-  // for (int i = 0; i < 100; i++)
-  //   printf("%d\n", randRange(1, 3));
+  int arr[] = {7, 6, 4, 3, 8, 2, 1, 5};
+  int length = (int)(sizeof(arr) / sizeof(arr[0]));
+
+  printf("Initial Array: ");
+  printArray(arr, length);
+  randomizedQucikSort(arr, 0, length - 1);
+  printf("After Quick sort: ");
+  printArray(arr, length);
+
+  int bigArrLength = 1000;
+  int bigArr[bigArrLength];
+  for (int i = 0; i < bigArrLength; i++)
+  {
+    bigArr[i] = bigArrLength - i;
+  }
+
+  printf("Big Array: ");
+  printArray(bigArr, bigArrLength);
+  randomizedQucikSort(bigArr, 0, bigArrLength - 1);
+  printf("After Quick sort: ");
+  printArray(bigArr, bigArrLength);
 }
